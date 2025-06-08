6e7e944555f3747edfd3886b184014131a6ad2ba
Greeshma370 <greeshmahvs370@gmail.com>
2025-06-08 19:04:22 +0530

    changed BrowserRouter to HashRouter

--- projects/ff-frontend/dist/assets/index-D1IH8uNl.js
+++ projects/ff-frontend/dist/assets/index-D1IH8uNl.js
@@ -1,4 +1,4 @@
-import{M as re,T as xt,R as N,C as x,a as ht,O as Z,b as X,E as j,c as Wo}from"./index-CIpSzfVF.js";import"./index-BvQbX75z.js";/**
+import{M as re,T as xt,R as N,C as x,a as ht,O as Z,b as X,E as j,c as Wo}from"./index-D4V05DgY.js";import"./index-Bo1eeJkZ.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
--- projects/ff-frontend/src/App.tsx
+++ projects/ff-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import Layout from './components/layout/Layout';
 import HomePage from './pages/HomePage';
